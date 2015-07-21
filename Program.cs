06d4f382c6d1e2cc3c2a76ae66889c4b86ebaea5
krav4enckoUM <iurakrav4encko@yandex.ru>
2015-07-21 20:22:05 +0500

    код

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Word word = new Word(); //Создаем экземпляр класса
+            Console.Write("Введите слово: ");
+            word.WordValue = Console.ReadLine();
+
+            Console.WriteLine("Слово наоборот: {0}", word.wordEndToBegin());
+            Console.WriteLine("Длина слова: {0}", word.WordMaxLength.ToString());
+            Console.WriteLine("Кол-во разных букв в слове: {0}", word.NumberOfDifferentLetters);
+
+            Console.Write("\nВведите символ для вставки: ");
+            string letter = Console.ReadLine();
+            Console.Write("Введите позицию для вставки: ");
+            Int32 position = Convert.ToInt32(Console.ReadLine());
+            string newWord = word.InsertLetter(letter, position);
+            Console.WriteLine("Слово после вставки: {0}", newWord);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Word.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_1
+{
+    class Word
+    {
+        public string WordValue { get; set; } //Свойство, хранящее введенное слово
+
+        public string wordEndToBegin() //Метод переворачивания слова
+        {
+            char[] letters = WordValue.ToCharArray(); //Преобразуем строку в массив символов
+            Array.Reverse(letters); //Реверсируем массив
+            return new string(letters); //переводим массив символов обратно в строку
+        }
+
+        public string InsertLetter(string letter, Int32 position) //Метод вставки символа в слово
+        {
+            if (position > WordValue.Length) //Проверка корректности введенных данных
+            {
+                return "Ошибка вставки!";
+            }
+            else
+            {
+                return WordValue.Insert(position - 1, letter); //Вставка символа в нужное место
+            }
+        }
+
+        public Int32 WordMaxLength
+        {
+            get { return WordValue.Length; } //Находим длину слова
+        }
+
+        public Int32 NumberOfDifferentLetters
+        {
+            get
+            {
+                Int32 i = 0;
+                var groups = WordValue.ToCharArray().GroupBy(c => c); //Лямбда-выражение для поиска разных букв
+                foreach (var char_group in groups)
+                    i++; //Подсчет кол-ва разных букв
+                return i;
+            }
+        }
+    }
+}
